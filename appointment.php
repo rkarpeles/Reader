567bd9deda5b5476a60b0dca6e92f8923c4ba1a5
Ryan Karpeles <ryan.karpeles@gmail.com>
2012-07-09 11:18:10 -0500

    Newest

---
+++ appointment.php
@@ -0,0 +1,97 @@
+<?php include('includes/header.php'); ?>
+		
+	<div id="content">
+		  
+		<?php include('includes/nav.php'); ?>
+
+		<div role="main"> 
+
+			<div class="container">
+				<div class="row">
+					<div class="twelvecol">
+						<h1>Make An Appointment</h1>	
+						<hr class="int">
+					</div>
+				</div>
+			</div>
+			
+			<div class="container">		
+				<div class="row">				
+					<div class="eightcol">	
+						<h3>To schedule an appointment, please give us a call at (847) 639-8008 or fill out the form below and we'll get back to you as soon as possible:</h3>
+						
+						<div class="sidebar-form full">
+
+						<div class="form-header">Make an Appointment</div>
+					
+							<form action="#" method="post" class="int-form">
+							
+								<div class="form-input">
+									 <label for="name">Name</label>
+									 <input type="text" name="name" id="name" value="" tabindex="1" />
+								</div>
+								
+								<div class="form-input">							 
+									 <label for="email">Email</label>
+									 <input type="email" name="email" id="email" value="" tabindex="1" />
+								</div>
+									 
+									 
+								<div class="form-input">	 
+									 <label for="name">Phone</label>
+									 <input type="tel" name="phone" id="phone" value="" tabindex="1" />				 
+								</div>
+								
+								<div class="form-input">	 
+									 <label for="date">Date requested:</label>
+									 <input type="date" name="date" id="date" value="" tabindex="1" />				 
+								</div>
+								
+								<div class="form-input">
+									<label for="textarea">Nature of your visit:</label>
+									<textarea cols="40" rows="6" name="textarea" id="textarea" tabindex="1"></textarea>
+								</div>				
+								
+
+								<div class="form-input">
+									<input type="submit" value="Submit" class="appt-btn" />
+								</div>
+								
+							</form>
+							
+						</div> <!-- End sidebar form -->
+						
+					</div>
+					
+					<div class="fourcol last">	
+						<h2>Contact Us</h2>
+						<p>
+							<strong>Phone</strong><br>
+							(847) 639-8008
+						</p>
+						<p>
+							<strong>Address</strong><br>
+							Stone Hill Shopping Center<br>
+							960 Illinois Route 22 Suite 206<br>
+							Fox River Grove, IL 60021
+						</p>
+						<p>
+							<em>Offices</em></br>
+							We are located near the intersection of Route 14 and Route 22. Our offices are upstairs in the Executive Offices, Suite 206.
+						</p>
+						<p>
+							<em>Parking</em></br>
+							Ample parking is available, along with elevator service.
+						</p>
+						
+					</div>
+				</div>
+			</div>
+			
+		</div> <!-- End of Main -->
+		
+		</div> <!-- End of Content -->
+	  
+		</div> <!-- End of Wrap -->
+		
+	<?php include('includes/footer.php'); ?>
